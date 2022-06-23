9eaecb97820fb3ae42e4c713655a671359c438e3
LuchoQQ <lucianosanchez125@hotmail.com>
2022-06-23 17:16:18 -0300

    endpoint /auth/me

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,46 @@
-const router = require('express').Router();
-const { User } = require('../models/index');
-const { body, validationResult } = require('express-validator');
-const bcrypt = require('bcrypt');
+const router = require("express").Router();
+const { User } = require("../models/index");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const { jwt } = require("jsonwebtoken");
+router.post(
+  "/login",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6 }),
+  (req, res) => {
+    if (!validationResult(req).isEmpty())
+      return res.status(401).json({ ok: false });
 
-router.post('/login',
-    body('email').isEmail(),
-    body('password').isLength({ min: 6 }),
-    (req, res) => {
-        if (!validationResult(req).isEmpty()) return res.status(401).json({ ok: false });
+    const { email, password } = req.body;
+    User.findOne({
+      where: { email },
+    }).then((user) => {
+      if (!user) return res.status(401).json({ ok: false });
+      if (!bcrypt.compareSync(password, user.password))
+        return res.status(401).json({ ok: false });
+      return res.json({ ok: true, user });
+    });
+  }
+);
 
-        const { email, password } = req.body;
-        User.findOne({
-            where: { email }
-        }).then(user => {
-            if (!user) return res.status(401).json({ ok: false });
-            if (!bcrypt.compareSync(password, user.password)) return res.status(401).json({ ok: false });
-            return res.json({ ok: true, user });
-        })
+const validateToken = require("./middlewares/validate");
+router.get("/me", async (req, res) => {
+  const token = req.headers.authorization;
 
-    })
+  if (!token) {
+    return res.status(401).json({
+      auth: false,
+      message: "No token provided",
+    });
+  }
+
+  const decoded = validateToken(token);
+  const user = await User.findByPk(decoded.user);
+  res.json({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    emai: user.email,
+  });
+});
 
 module.exports = router;
