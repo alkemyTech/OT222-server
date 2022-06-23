bdf5bc051f0317389aa29faaa149bfb346153124
LuchoQQ <lucianosanchez125@hotmail.com>
2022-06-23 16:19:58 -0300

    fix endpoint error

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const Model = require("../models/index");
-const { check } = require("express-validator");
-const bcrypt = require("bcrypt");
-const { createUser } = require('../controllers/users.controllers');
-const userValidationSchema = require('../validations/userValidationSchema')
-
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// Create a new user
-router.post('/auth/register', userValidationSchema, createUser)
+
 
 module.exports = router;
