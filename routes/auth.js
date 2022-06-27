0e5c39981a56480f6e372a93283509b9b4ab4b15
LuchoQQ <lucianosanchez125@hotmail.com>
2022-06-27 18:15:02 -0300

    duplicate login removed

--- routes/auth.js
+++ routes/auth.js
@@ -3,27 +3,11 @@ const { User } = require("../models/index");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const { jwt } = require("jsonwebtoken");
-router.post(
-  "/login",
-  body("email").isEmail(),
-  body("password").isLength({ min: 6 }),
-  (req, res) => {
-    if (!validationResult(req).isEmpty())
-      return res.status(401).json({ ok: false });
-
-    const { email, password } = req.body;
-    User.findOne({
-      where: { email },
-    }).then((user) => {
-      if (!user) return res.status(401).json({ ok: false });
-      if (!bcrypt.compareSync(password, user.password))
-        return res.status(401).json({ ok: false });
-      return res.json({ ok: true, user });
-    });
-  }
-);
-
 const validateToken = require("./middlewares/validate");
+
+
+
+// auth me
 router.get("/me", async (req, res) => {
   const token = req.headers.authorization;
 
