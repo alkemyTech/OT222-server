7107a55c46fab58a2ce91fd9f03e27a4f9050d9d
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-06-27 13:27:52 -0300

    Code simplified

--- routes/middlewares/AdminAuth.js
+++ routes/middlewares/AdminAuth.js
@@ -8,13 +8,8 @@ const AdminAuth = (req, res, next) => {
 
     // Replace by jwt verify middleware when OT222-36 merged
     jwt.verify(token.replace("Bearer ", ""), config.secret, (err, decoded) => {
-        if (err) {
-            console.log(err)
-            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        }
-        if (decoded.roleId !== 'admin') {
-            return res.status(401).send({ auth: false, message: 'Unauthorized' });
-        }
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        if (decoded.roleId !== 'admin') return res.status(401).send({ auth: false, message: 'Unauthorized' });
         next();
     });
 }
