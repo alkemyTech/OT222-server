0883c3ca9447b40024656fb1e1d7143ccb2949ec
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-06-27 23:59:46 -0300

    roleId validation from database query

--- routes/middlewares/AdminAuth.js
+++ routes/middlewares/AdminAuth.js
@@ -1,16 +1,22 @@
 const jwt = require('jsonwebtoken');
 const config = require('../../config/config').development;
+const { User } = require("../../models/index");
+
 
 const AdminAuth = (req, res, next) => {
     console.log(req.headers);
     const token = req.headers.authorization;
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
 
-    // Replace by jwt verify middleware when OT222-36 merged
     jwt.verify(token.replace("Bearer ", ""), config.secret, (err, decoded) => {
         if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        if (decoded.roleId !== 'admin') return res.status(401).send({ auth: false, message: 'Unauthorized' });
-        next();
+        console.log(decoded.id)
+        User.findByPk(decoded.id)
+            .then(user => {
+                if (!user || user.roleId !== 1) return res.status(401).send({ auth: false, message: 'Unauthorized' })
+                next();
+            })
+            .catch(err => console.log(err))
     });
 }
 
