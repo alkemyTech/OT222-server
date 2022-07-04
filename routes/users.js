a1bf8b6233feba7c08ddd3f710d8e6482c969cee
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-07-04 17:56:48 -0300

    Missing dependency error fixed

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 
-///const usersControllers = require('../controllers/users');
+const usersControllers = require('../controllers/users');
 
-const { getUser }  = require("../controllers/user.controllers");
+const { getUser } = require("../controllers/user.controllers");
 
 /* GET users listing. */
-router.get("/",getUser);
+router.get("/", getUser);
 
 
 
