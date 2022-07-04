20f1245bcecfe1be17c3ee4c10b372772c02ba4d
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-07-04 17:52:03 -0300

    Duplicates deleted and content organized

--- app.js
+++ app.js
@@ -1,50 +1,21 @@
+require('dotenv').config();
 
 const createError = require('http-errors');
 const express = require('express');
+const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-require('dotenv').config();
 
-// Routers
+//Routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 const mailRouter = require('./routes/mail');
-
 const organizationsRouter = require("./routes/organizations")
-const news = require('./routes/news')
+const newsRouter = require('./routes/news')
 
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Routers
-const indexRouter = require("./routes/index");
-const newsRouter = require("./routes/news");
-const usersRouter = require("./routes/users");
-const authRouter = require("./routes/auth");
-const mailRouter = require("./routes/mail");
-const organizationsRouter = require("./routes/organizations");
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-
-//Routes
-
-app.use(logger("dev"));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-
-//Routes
 app.use("/", indexRouter);
 app.use("/news", newsRouter);
 app.use("/users", usersRouter);
@@ -52,12 +23,22 @@ app.use("/auth", authRouter);
 app.use("/mail", mailRouter);
 app.use("/organizations", organizationsRouter);
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
@@ -67,10 +48,6 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
-});
-
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server initialized on port ${process.env.SERVER_PORT} `);
 });
 
 module.exports = app;
