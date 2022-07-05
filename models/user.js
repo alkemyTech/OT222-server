a962e2a6f087a6d815e26d8fbb608e0a98a05684
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-07-05 02:57:05 -0300

    Users model renamed back to User

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
+  class User extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Users.belongsTo(models.Role, { as: 'role' });
+      User.belongsTo(models.Role, { as: 'role' });
     }
   }
-  Users.init(
+  User.init(
     {
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
@@ -23,8 +23,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'Users',
+      modelName: 'User',
     }
   );
-  return Users;
+  return User;
 };
