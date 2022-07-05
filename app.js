d5b09318ba10748788fddfb3777b06f8d6ff9ce9
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-07-05 02:54:16 -0300

    Routes moved below middlewares

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+
 //Routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -23,18 +36,6 @@ app.use("/auth", authRouter);
 app.use("/mail", mailRouter);
 app.use("/organizations", organizationsRouter);
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-//Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
