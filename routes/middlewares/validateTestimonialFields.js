e7ea5fbcf7160afcf4f3034015af066f6e01beae
Noe <noeliapaz676@gmail.com>
2022-07-05 18:06:46 -0300

    Remove unnecesary validations

--- routes/middlewares/validateTestimonialFields.js
+++ routes/middlewares/validateTestimonialFields.js
@@ -3,14 +3,10 @@ const { validation } = require('../../util/helper');
 
 const validateTestimonialFields = [
   check('name', 'The name cannot be empty, and must be letters')
-    .exists()
-    .isLength({ min: 3 })
     .not()
     .isEmpty()
     .isString(),
   check('content', 'The content cannot be empty, and must be letters')
-    .exists()
-    .isLength({ min: 3 })
     .not()
     .isEmpty()
     .isString(),
