456c883b3ecf82cfb9b10e55d70fe9140f51efb3
MagdalenaCasas <maguicasas33@gmail.com>
2022-07-07 17:57:13 -0300

    endpoint put testimonials

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const testimonialsControllers = require('../controllers/testimonials');
 
 router.use('/', testimonialsControllers);
+router.use('/:id', testimonialsControllers);
 
 module.exports = router;
