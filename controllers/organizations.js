4be952231d84d2e981ba6cd7f67e707802f559d5
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-10 02:54:00 -0500

    social media in the response organization added

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -1,24 +1,30 @@
-
-
-const getPublic = async (req,res)=>{
-/* const{ name, image, phone, adress, elcomeText} = await Public.findOne({
+// models
+const { organizations } = require("../models")
+const getPublic = async (req, res) => {
+  /* const{ name, image, phone, adress, elcomeText} = await Public.findOne({
     were:{
         id:1,
     },
 })*/
 
-    const results = {
-        id:1,
-        name: "somos mas",
-        image: "hdbflajgssbd,hvg",
-        phone: "563248723645984",
-        adress: "dmenfj 19384",
-        welcomeText: "sdfbdjHGFBNDCVKJGF<ANBSDFVKfdmnbcKHGF",
-    }
+  try {
+    const organization = await organizations.findAll()
+    res.status(200).json({ organizations: organization })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
 
-    res.status(200).json({results})
+  //   const results = {
+  //     id: 1,
+  //     name: "somos mas",
+  //     image: "hdbflajgssbd,hvg",
+  //     phone: "563248723645984",
+  //     adress: "dmenfj 19384",
+  //     welcomeText: "sdfbdjHGFBNDCVKJGF<ANBSDFVKfdmnbcKHGF",
+  //   }
+  //   res.status(200).json({ results })
 }
 
-module.exports={
-    getPublic,
+module.exports = {
+  getPublic,
 }
