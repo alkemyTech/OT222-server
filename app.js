d81fc3a7d0c83f13892233f4c157e7d2f2462dd9
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-07-14 17:54:59 -0300

    /members GET, POST, PUT and DELETE endpoints

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const newsRouter = require('./routes/news');
 const testimonialsRouter = require('./routes/testimonials');
 const activitiesRouter = require('./routes/activities');
 const contactsRouter = require('./routes/contacts');
+const membersRouter = require('./routes/members');
 
 app.use(cors());
 
@@ -43,6 +44,7 @@ app.use('/organizations', organizationsRouter);
 app.use('/testimonials', testimonialsRouter);
 app.use('/activities', activitiesRouter);
 app.use('/contacts', contactsRouter);
+app.use('/members', membersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
