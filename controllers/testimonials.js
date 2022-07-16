6ccb0fc5210e39bb52a9aecfa9ac7389fbe0a549
Noe <noeliapaz676@gmail.com>
2022-07-16 19:39:24 -0300

    Add DELETE method to testimonials

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -2,29 +2,41 @@ var express = require('express');
 var router = express.Router();
 const { Testimonials } = require('../models');
 const validateTestimonialFields = require('../routes/middlewares/validateTestimonialFields');
-const validateTestimonialId = require("../routes/middlewares/ValidateTestimonialId");
+const validateTestimonialId = require('../routes/middlewares/ValidateTestimonialId');
 
 router.post('/', validateTestimonialFields, async (req, res) => {
   const data = await Testimonials.create(req.body);
   res.json(data);
 });
 
-router.put('/:id', validateTestimonialFields,validateTestimonialId, async (req, res) => {
-  const idParam = req.params.id;
-  try {
-    
-    await Testimonials.update(
-      {
-        name: req.body.name,
-        content: req.body.content,
-      },
-      {where:{id: idParam}
-    })
-    const testimonial = await Testimonials.findOne({where:{id:idParam}})
-    res.status(200).json(testimonial);
-  } catch (error) {
-    res.status(400).json({error: error.message})
+router.put(
+  '/:id',
+  validateTestimonialFields,
+  validateTestimonialId,
+  async (req, res) => {
+    const idParam = req.params.id;
+    try {
+      await Testimonials.update(
+        {
+          name: req.body.name,
+          content: req.body.content,
+        },
+        { where: { id: idParam } }
+      );
+      const testimonial = await Testimonials.findOne({
+        where: { id: idParam },
+      });
+      res.status(200).json(testimonial);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
+);
+
+router.delete('/:id', async (req, res) => {
+  Testimonials.destroy({ where: { id: req.params.id } })
+    .then(() => res.send({ success: 'El testimonio se ha eliminado' }))
+    .catch(err => res.status(400).send(err));
 });
 
 module.exports = router;
