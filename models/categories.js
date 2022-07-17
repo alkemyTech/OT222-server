89183169dc06482dc0e9d6287252aea16da24aa2
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-17 00:15:22 -0500

    models categories refactored

--- models/categories.js
+++ models/categories.js
@@ -1,7 +1,7 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict"
+const { Model } = require("sequelize")
 module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
+  class Categories extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Category.init(
+  Categories.init(
     {
       name: DataTypes.STRING,
       description: DataTypes.STRING,
@@ -19,9 +19,9 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'Category',
+      modelName: "Categories",
       paranoid: true,
     }
-  );
-  return Category;
-};
+  )
+  return Categories
+}
