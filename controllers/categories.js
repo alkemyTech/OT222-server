c6bc238f22d8794553473a3787981c2544c71396
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-16 22:45:16 -0500

    code refactored and and creating the post to route in order to create categories, validating name and description exist and type must be a string

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,47 +1,83 @@
-var express = require('express');
-var router = express.Router();
-const { Category } = require('../models');
+const { Category } = require("../models")
 
-router.get('/', async (req, res) => {
-  await Category.findAll()
-    .then(categories => {
-      res.send(categories);
-    })
-    .catch(err => res.status(400).send(err));
-});
-
-router.get('/:id', async (req, res) => {
-  await Category.findOne({ where: { id: req.params.id } })
-    .then(category => {
-      res.send(category);
-    })
-    .catch(err => res.status(400).send(err));
-});
-
-router.put('/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, description } = req.body;
-  console.log('id', id);
-
-  const category = await Category.findOne({ where: { id: req.params.id } });
-
-  if (!category) {
-    res.json({ message: 'Category not found' });
-  }
-
-  await Category.update(
-    {
+const createCategory = async (req, res) => {
+  try {
+    const { name, description } = req.body
+    // validate that name exists
+    if (!name || !description)
+      return res.status(400).json({
+        message: "Name is required and description are required",
+      })
+    // validate name and description string
+    if (typeof name !== "string" || typeof description !== "string")
+      return res.status(400).json({
+        message: "Name and description must be string",
+      })
+    const category = await Category.create({
       name: name,
       description: description,
-    },
-    {
-      where: {
-        id: id,
-      },
-    }
-  )
-    .then(category => res.status(200).json({ message: 'Update successful!' }))
-    .catch(err => res.status(400).json({ error: err.message }));
-});
+    })
+    res.status(201).json({
+      category,
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
 
-module.exports = router;
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.findAll()
+    res.status(200).json({
+      categories,
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const getCategoriesById = async (req, res) => {
+  try {
+    const category = await Category.findOne({ where: { id: req.params.id } })
+    if (!category)
+      return res.status(404).json({ message: "Category not found" })
+    res.status(200).json({
+      category,
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const editCategoryById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { name, description } = req.body
+    const category = await Category.findOne({ where: { id: id } })
+    if (!category)
+      return res.status(404).json({ message: "Category not found" })
+    const categoryUpdated = await category.update(
+      {
+        name: name,
+        description: description,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    )
+    res.status(200).json({
+      categoryUpdated,
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+module.exports = {
+  createCategory,
+  getCategories,
+  getCategoriesById,
+  editCategoryById,
+}
