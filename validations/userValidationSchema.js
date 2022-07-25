2ec3f4677eb50cb87654253fdfcf00c7ea64b8ef
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-07-25 16:42:36 -0300

    Auth validations improved

--- validations/userValidationSchema.js
+++ validations/userValidationSchema.js
@@ -2,17 +2,24 @@ const { check } = require("express-validator");
 
 const userValidationSchema = [
   check("firstName")
-    .isLength({ min: 3, max: 15 })
+    .not().isEmpty()
     .withMessage("First name is required")
+    .isLength({ min: 3, max: 15 })
     .withMessage("First name must be between 3 and 15 characters"),
   check("lastName")
-    .isLength({ min: 3, max: 15 })
+    .not().isEmpty()
     .withMessage("Last name is required")
+    .isLength({ min: 3, max: 15 })
     .withMessage("Last name must be between 3 and 15 characters"),
-  check("email").isEmail().withMessage("Email is required"),
+  check("email")
+    .not().isEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Email must be a valid email address"),
   check("password")
-    .isLength({ min: 8, max: 15 })
+    .not().isEmpty()
     .withMessage("Password is required")
+    .isLength({ min: 8, max: 15 })
     .withMessage("Password must be between 8 and 15 characters"),
 ];
 
